2d660ad937532467cbd248f54e0fa5ece6e5fd27
Thanathip Limna <boatkrap@gmail.com>
2011-11-10 17:36:12 +0700

    add OperatingQueue and CommandLog

---
+++ system.py
@@ -0,0 +1,28 @@
+'''
+Created on Nov 8, 2011
+
+@author: boatkrap
+'''
+
+from mongoengine import *
+import datetime
+
+class OperatingQueue(Document):
+    meta = {'collection': 'operating_queue'}
+    
+    camera  = ReferenceField("Camera")
+    action  = StringField(required=True, default='Waiting')
+    date    = DateTimeField(required=True, default=datetime.datetime.now())
+    user    = ReferenceField("User")
+    
+class CommandLog(Document):
+    meta = {'collection': 'command_log'}
+    
+    id      = SequenceField(required=True)
+    camera  = ReferenceField("Camera")
+    action  = StringField(required=True, default='Waiting')
+    user    = ReferenceField("User")
+    attributes      = DictField()
+    compute_node    = EmbeddedDocumentField("ComputeNode")
+    command_date    = DateTimeField(required=True, default=datetime.datetime.now())
+    complete_date   = DateTimeField(required=True, default=datetime.datetime.now())
