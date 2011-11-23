3426a10168cab80fb94a7982c43f6b8018697aff
Thanathip Limna <boatkrap@gmail.com>
2011-11-23 13:38:18 +0700

    chang compute node name

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 from users import User, Group
 from cameras import CameraOperating, Camera, CameraModel, Manufactory
 from image_processors import ImageProcessor
-from compute_node import CPUInfomation, MemoryInfomation, ComputeNode
+from compute_nodes import CPUInfomation, MemoryInfomation, ComputeNode
 from system import CameraCommandQueue, CommandLog
 
 from mongoengine import connect
