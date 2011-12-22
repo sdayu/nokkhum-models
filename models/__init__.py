e12499924c270f905d02bfcd68d980b69924f853
Thanathip Limna <boatkrap@gmail.com>
2011-12-22 19:41:59 +0700

    change model to common and add message to this project

---
+++ models/__init__.py
@@ -0,0 +1,10 @@
+from users import User, Group
+from cameras import CameraOperating, Camera, CameraModel, Manufactory
+from image_processors import ImageProcessor
+from compute_nodes import CPUInfomation, MemoryInfomation, ComputeNode
+from system import CameraCommandQueue, CommandLog
+
+from mongoengine import connect
+
+def initial(setting):
+    connect(setting.get('mongodb.name'), host=setting.get('mongodb.url'))
