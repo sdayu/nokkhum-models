abd826865ae27ff10634254e77f6bf42a9e76202
Thanathip Limna <boatkrap@gmail.com>
2011-12-23 12:45:41 +0700

    add producer factory

--- messages/producer.py
+++ messages/producer.py
@@ -9,7 +9,8 @@ from __future__ import with_statement
 from kombu.common import maybe_declare
 from kombu.pools import producers
 
-    
+from . import connection
+
 class Producer:
     def __init__(self, connection):
         self._connection = connection
@@ -19,3 +20,15 @@ class Producer:
             
     def send(self, message):
         self._producer.publish(message)
+        
+class ProducerFactory:
+    def __init__(self):
+        self.connection = None
+        self.producer = None
+        
+    def get_producer(self, name):
+        if name == "nokkhum_compute.update_status":
+            if self.connection is None:
+                self.connection = connection.Connection()
+            self.producer = Producer(connection)
+            return self.producer
