68c48e241bbefc22bd35c09c576813f1b01befde
Thanathip Limna <boatkrap@gmail.com>
2011-12-27 11:49:43 +0700

    add consumer

---
+++ messages/consumer.py
@@ -0,0 +1,53 @@
+'''
+Created on Dec 23, 2011
+
+@author: boatkrap
+'''
+import kombu
+import kombu.messaging
+from kombu import Exchange
+
+
+from . import connection
+from . import queues
+
+class Consumer:
+
+    def __init__(self, exchange_name, channel, routing_key, callback=None):
+        self.exchange_name = exchange_name
+        self.callback = callback
+        self.routing_key = routing_key
+        self._consumer = None
+        self.reconnect(channel)
+
+    def reconnect(self, channel):
+        exchange = Exchange(self.exchange_name, type="direct", durable=True)
+        queue = queues.QueueFactory().get_queue(exchange, self.routing_key)
+        queue(channel).declare()
+        self._consumer = kombu.messaging.Consumer(channel, queue, callbacks=self.callback)
+    
+    def register(self, callback):
+        self._consumer.register_callback(callback)
+        self.callback = callback
+
+class ConsumerFactory:
+    def __init__(self):
+        self.connection = None
+        self.consumer = None
+        
+    def get_consumer(self, name):
+        if name == "nokkhum_compute.update_status":
+            routing_key = "nokkhum_compute.update_status"
+            if self.connection is None:
+                self.connection = connection.default_connection.get_broker_connection()
+            
+            channel = self.connection.channel()
+            
+            self.consumer = Consumer("nokkunm_compute", channel, routing_key)
+            return self.consumer
+    
+    def get_connection(self):
+        if self.connection is None:
+            self.connection = connection.default_connection.get_broker_connection()
+            
+        return self.connection
