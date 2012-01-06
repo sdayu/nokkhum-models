000de2c371f86dd68873749a3fbf55d3d74739d4
Thanathip Limna <boatkrap@gmail.com>
2012-01-06 15:57:08 +0700

    begin add topic publisher

--- messages/publisher.py
+++ messages/publisher.py
@@ -36,18 +36,37 @@ class Publisher:
     def send(self, message):
         self._producer.publish(message)
         
+class TopicPublisher(Publisher):
+    
+    def reconnect(self, channel):
+        exchange = Exchange(self.exchange_name, type="topic", durable=True)
+        queue = queues.QueueFactory().get_queue(exchange, self.routing_key)
+        queue(channel).declare()
+        
+        self._producer = kombu.messaging.Producer(exchange=exchange,
+            channel=channel, serializer="json", 
+            routing_key=self.routing_key)  
+        
 class PublisherFactory:
     def __init__(self):
         self.connection = None
-        self.producer = None
-        
+        self.publisher_pool = {}
+    
+    
     def get_producer(self, name):
+        
+        if self.connection is None:
+            self.connection = connection.default_connection.get_broker_connection()
+                
         if name == "nokkhum_compute.update_status":
             routing_key = "nokkhum_compute.update_status"
-            if self.connection is None:
-                self.connection = connection.default_connection.get_broker_connection()
+            if routing_key in self.publisher_pool.keys():
+                return self.publisher_pool["nokkhum_compute.update_status"]
             
             channel = self.connection.channel()
             
-            self.producer = Publisher("nokkunm_compute", channel, routing_key)
-            return self.producer
+            publisher = Publisher("nokkunm_compute", channel, routing_key)
+            self.publisher_pool["nokkhum_compute.update_status"] = publisher
+            return publisher
+        
+        elif name == "nokkhum_compute.update_status":
