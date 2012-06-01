0a630c0bb5fac1576e86370dc0d8e485f6e67662
Thanathip Limna <boatkrap@gmail.com>
2012-06-01 12:14:34 +0700

    sparate module

--- __init__.py
+++ __init__.py
@@ -1,1 +1,10 @@
-from . import messaging, models
+from users import User, Group
+from cameras import CameraOperating, Camera, CameraModel, Manufactory
+from image_processors import ImageProcessor
+from compute_nodes import CPUInfomation, MemoryInfomation, ComputeNode
+from system import CameraCommandQueue, CommandLog
+
+from mongoengine import connect
+
+def initial(setting):
+    connect(setting.get('mongodb.db_name'), host=setting.get('mongodb.host'))
