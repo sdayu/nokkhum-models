ffcae3384d3c89e56e41f1d2ef32acaff88e80df
Thanathip Limna <boatkrap@gmail.com>
2012-08-06 18:06:25 +0700

    modify compute node for support vm

--- compute_nodes.py
+++ compute_nodes.py
@@ -10,18 +10,44 @@ class MemoryInfomation(EmbeddedDocument):
     total   = IntField(required=True, default=0)
     used    = IntField(default=0)
     free    = IntField(default=0)
+
+class VMInstance(EmbeddedDocument):
+    name            = StringField(max_length=100, required=True)
     
+    instance_id     = StringField(required=True)
+    image_id        = StringField(required=True)
+    
+    # vm information
+    kernel          = StringField()
+    ramdisk         = StringField()   
+    instance_type   = StringField(required=True)
+    
+    ip_address      = StringField()
+    private_ip_address = StringField()
+    
+    start_instance_date     = DateTimeField(required=True, default=datetime.datetime.now)
+    terminate_instance_date = DateTimeField()
+
+
 class ComputeNode(Document):
     meta = {'collection': 'compute_nodes'}
     
     name    = StringField(max_length=100, required=True)
-    system  = StringField(max_length=100, required=True)
+    system  = StringField(max_length=100)
     host    = StringField(max_length=100, required=True)
-    machine = StringField(max_length=100, required=True)
+    machine = StringField(max_length=100)
     cpu     = EmbeddedDocumentField("CPUInfomation", required=True)
     memory  = EmbeddedDocumentField("MemoryInfomation", required=True)
     
     create_date = DateTimeField(required=True, default=datetime.datetime.now)
     update_date = DateTimeField(required=True, default=datetime.datetime.now)
 
+    vm      = EmbeddedDocumentField(VMInstance)
+    
+    def is_vm(self):
+        if self.vm is None:
+            return False
+        
+        return True
+        
 
