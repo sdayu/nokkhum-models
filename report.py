234c7554f511de01ee713d870d262f99c8cbbaa7
Thanathip Limna <boatkrap@gmail.com>
2012-08-08 12:49:14 +0700

    add compute node report

---
+++ report.py
@@ -0,0 +1,27 @@
+'''
+Created on Aug 7, 2012
+
+@author: boatkrap
+'''
+from mongoengine import *
+import datetime
+
+class CameraProcessStatus(EmbeddedDocument):
+    cpu     = FloatField(default=0)
+    ram     = IntField(default=0)
+    threads = IntField(default=0)
+    
+    camera  = ReferenceField("Camera")
+
+class ComputeNodeReport(Document):
+    meta = {'collection': 'compute_node_report'}
+    
+    compute_node    = ReferenceField("ComputeNote")
+    
+    report_date     = DateTimeField(required=True, default=datetime.datetime.now)
+    
+    cpu             = EmbeddedDocumentField("CPUInfomation", required=True)
+    memory          = EmbeddedDocumentField("MemoryInfomation", required=True)
+      
+    camera          = ListField(EmbeddedDocumentField(CameraProcessStatus))
+    
