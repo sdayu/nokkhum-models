695b61849490811a5e1807130166a9d12df00e28
Thanathip Limna <boatkrap@gmail.com>
2012-08-22 22:16:56 +0700

    add VMInstance

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@ from users import User, Group
 from projects import Project
 from cameras import CameraOperating, Camera, CameraModel, Manufactory
 from image_processors import ImageProcessor
-from compute_nodes import CPUInfomation, MemoryInfomation, ComputeNode
+from compute_nodes import CPUInfomation, MemoryInfomation, ComputeNode, VMInstance
 from system import CameraCommandQueue, CommandLog, CameraRunningFail
 from report import ComputeNodeReport, CameraProcessStatus
 
