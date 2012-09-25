b9b4cea13713f5272ff3dae7e512905215228565
Thanathip Limna <boatkrap@gmail.com>
2012-09-25 19:25:01 +0700

    change __init__.py to support python3

--- __init__.py
+++ __init__.py
@@ -1,10 +1,10 @@
-from users import User, Group
-from projects import Project
-from cameras import CameraOperating, Camera, CameraModel, Manufactory
-from image_processors import ImageProcessor
-from compute_nodes import CPUInfomation, MemoryInfomation, ComputeNode, VMInstance
-from system import CameraCommandQueue, CommandLog, CameraRunningFail
-from report import ComputeNodeReport, CameraProcessStatus
+from .users import User, Group
+from .projects import Project
+from .cameras import CameraOperating, Camera, CameraModel, Manufactory
+from .image_processors import ImageProcessor
+from .compute_nodes import CPUInfomation, MemoryInfomation, ComputeNode, VMInstance
+from .system import CameraCommandQueue, CommandLog, CameraRunningFail
+from .report import ComputeNodeReport, CameraProcessStatus
 
 from mongoengine import connect
 
