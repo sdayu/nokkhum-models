02ad523c80196c9be3fac08efbae3adba21bc04c
Thanathip Limna <boatkrap@gmail.com>
2013-01-15 14:29:57 +0700

    fix embedded document bug

--- projects.py
+++ projects.py
@@ -14,7 +14,7 @@ class CollboratorPermission(EmbeddedDocument):
 
 class Collaborator(EmbeddedDocument):
     user        = ReferenceField("User", required=True)
-    camera_permissions = ListField(EmbeddedField(CollboratorPermission))
+    camera_permissions = ListField(EmbeddedDocumentField(CollboratorPermission))
     
     permissions = ListField(StringField(default='view'))
     create_date = DateTimeField(required=True, default=datetime.datetime.now)
@@ -32,7 +32,7 @@ class Project(Document):
     ip_address  = StringField(max_length=100, required=True, default='0.0.0.0')
     
     owner       = ReferenceField("User", required=True)
-    collaborators = ListField(EmbeddedField(Collaborator))
+    collaborators = ListField(EmbeddedDocumentField(Collaborator))
     
     def get_camera_number(self):
         return Camera.objects(project = self).count()
